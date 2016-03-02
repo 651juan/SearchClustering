a1ae2493785a30413b28a64aad98eb9870543e53
Adrian <apapadrian@gmail.com>
2016-03-02 11:50:01 +0100

    Waits for page to load

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
 //called when tab is changed
 function checkForValidUrl(tabId, changeInfo, tab) {
 	//if url has google in it show extension icon
-	console.log(changeInfo.url);
 	if (tab.url.indexOf('google.com') >= 0) {
 		chrome.pageAction.show(tabId);
-		chrome.tabs.executeScript(null, {file: "getTitle.js"});
+		if(changeInfo.status == "complete") {
+			chrome.tabs.executeScript(null, {file: "getTitle.js"});
+		}
 	}
 };
 
